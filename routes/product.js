bd92ad1d7e56a50a444557b168b8b940b4938ac6
Alexandru Cotin <a.cotin96@gmail.com>
2020-04-20 17:20:42 +0200

    added related products

--- routes/product.js
+++ routes/product.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const {
-  create,
-  productById,
-  read,
-  remove,
-  update
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated
 } = require("../controllers/product");
-const { userById } = require("../controllers/user.js");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete(
-  "/product/:productId/:userId",
-  requireSignin,
-  isAuth,
-  isAdmin,
-  remove
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
 );
 router.put(
     "/product/:productId/:userId",
@@ -27,6 +29,9 @@ router.put(
     isAdmin,
     update
 );
+
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
 
 router.param("userId", userById);
 router.param("productId", productById);
